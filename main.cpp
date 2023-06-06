cfaa5907ab93cb510a5e29321baeb970d1384475
Junius HO <125662406+juniusho@users.noreply.github.com>
2023-06-06 13:31:28 +0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -19,6 +19,31 @@ int start(){
     return choice;
 }
 
+void display(int M[][10],int rows, int columns){
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < columns; j++){
+            cout << M[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void addition(int M1[][10], int M2[][10], int result[][10], int rows, int columns){
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < columns; j++){
+            result[i][j] = M1[i][j] + M2[i][j];
+        }
+    }
+}
+
+void subtraction(int M1[][10], int M2[][10], int result[][10], int rows, int columns){
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < columns; j++){
+            result[i][j] = M1[i][j] - M2[i][j];
+        }
+    }
+}
+
 int main(){
     bool flag=1;
     int choice=0;
@@ -26,7 +51,39 @@ int main(){
     while(flag==1){
         choice = start();
         switch(choice){
-            case 1:{}
+            case 1:{
+                int rows, columns;
+                cout << "Enter the number of rows: ";
+                cin >> rows;
+                cout << "Enter the number of columns: ";
+                cin >> columns;
+                int M1[rows][10], M2[rows][10], result[rows][10];
+                cout << "Enter the elements of Matrix 1:" << endl;
+                for (int i = 0; i < rows; i++) {
+                    for (int j = 0; j < columns; j++) {
+                        cin >> M1[i][j];
+                    }
+                }
+                cout << "Enter the elements of Matrix 2:" << endl;
+                for (int i = 0; i < rows; i++) {
+                    for (int j = 0; j < columns; j++) {
+                        cin >> M2[i][j];
+                    }
+                }
+                cout << "\nMatrix 1:" << endl;
+                display(M1, rows, columns);
+
+                cout << "\nMatrix 2:" << endl;
+                display(M2, rows, columns);
+
+                cout << "\nAddition of Matrix 1 and Matrix 2:" << endl;
+                addition(M1, M2, result, rows, columns);
+                display(result, rows, columns);
+
+                cout << "\nSubtraction of Matrix 1 and Matrix 2:" << endl;
+                subtraction(M1, M2, result, rows, columns);
+                display(result, rows, columns);
+            }
             case 2:{}
             case 3:{}
             case 4:{}
